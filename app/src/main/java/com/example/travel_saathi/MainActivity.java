f6fc511a50ee791b348ff6e2690f7d861e980379
abhijeet <abhijeetpandey.dhn@gmail.com>
2023-10-28 23:30:32 +0530

    added inimation file

--- app/src/main/java/com/example/travel_saathi/MainActivity.java
+++ app/src/main/java/com/example/travel_saathi/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.travel_saathi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+TextView txtAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtAnim = findViewById(R.id.app_name);
+
+        // Animation move = AnimationUtils.LoadAnimation( this,R.anim.move);
+
+
+
+
+
     }
 }
