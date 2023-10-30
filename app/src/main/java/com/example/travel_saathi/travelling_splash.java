eb50452fa1e75262e58e43cc0d568d6985958141
abhijeet <abhijeetpandey.dhn@gmail.com>
2023-10-30 12:24:12 +0530

    created food and teavelling splash screen

---
+++ app/src/main/java/com/example/travel_saathi/travelling_splash.java
@@ -0,0 +1,14 @@
+package com.example.travel_saathi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class travelling_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travelling_splash);
+    }
+}
