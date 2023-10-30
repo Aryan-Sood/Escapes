71501bd19e4fc3e0ea3040f5f9f2e08d4587a979
Aryan <aryansood12@gmail.com>
2023-10-31 00:52:57 +0530

    Earth rotation animation

--- app/src/main/java/com/example/travel_saathi/SplashScreen.java
+++ app/src/main/java/com/example/travel_saathi/SplashScreen.java
@@ -3,14 +3,32 @@ package com.example.travel_saathi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView earthImage;
+
+    Animation rotate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        earthImage = findViewById(R.id.earthImage);
+
+        rotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.earth_rotation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                earthImage.startAnimation(rotate);
+            }
+        },1000);
 
 
     }
