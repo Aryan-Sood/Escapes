e913f20bb7321647d04c2778240eb2a921e4667a
abhijeet <abhijeetpandey.dhn@gmail.com>
2023-11-01 01:09:45 +0530

    Created Avtar_selection page nad added few animation

---
+++ app/src/main/java/com/example/travel_saathi/Avtar_selection.java
@@ -0,0 +1,14 @@
+package com.example.travel_saathi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Avtar_selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avtar_selection);
+    }
+}
