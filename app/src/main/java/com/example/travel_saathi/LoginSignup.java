c9f59321d1f0a0a9900f6fae09424a376692d6ae
Aryan <aryansood12@gmail.com>
2023-11-11 19:32:49 +0530

    Changes: 1. Working on Login/Signup Page

--- app/src/main/java/com/example/travel_saathi/LoginSignup.java
+++ app/src/main/java/com/example/travel_saathi/LoginSignup.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class LoginSignup extends AppCompatActivity {
@@ -12,16 +15,31 @@ public class LoginSignup extends AppCompatActivity {
 
     ImageView loginBackImage;
 
+    Animation loginHideAnimation;
+
+    ImageView upButton, downButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup);
 
         loginBackImage = findViewById(R.id.login_background_image);
+        upButton = findViewById(R.id.up_arrow);
+
 
         getScreenHeight();
+
         loginBackImage.getLayoutParams().height=height/2;
         loginBackImage.requestLayout();
+
+        upButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginHideAnimation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.login_hide_animation);
+                loginBackImage.startAnimation(loginHideAnimation);
+            }
+        });
 
     }
 
