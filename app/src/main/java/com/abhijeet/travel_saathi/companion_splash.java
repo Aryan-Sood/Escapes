3186ed8dd05a2991fab3391c61bce25c8f0688c1
Vibhor Mathur <matvibhor24@gmail.com>
2023-12-03 14:14:56 +0530

    edited splash screens

--- app/src/main/java/com/abhijeet/travel_saathi/companion_splash.java
+++ app/src/main/java/com/abhijeet/travel_saathi/companion_splash.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.abhijeet.travel_saathi.R;
-
-
-public class travelling_splash extends AppCompatActivity {
+public class companion_splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_travelling_splash);
+        setContentView(R.layout.activity_companion_splash);
     }
 }
