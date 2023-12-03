2a3adbb9d91a62dd4d5235e0269cb254d084ec94
Vibhor Mathur <matvibhor24@gmail.com>
2023-12-03 23:33:16 +0530

    created signup_successfully

---
+++ app/src/main/java/com/abhijeet/travel_saathi/Signup_successfully.java
@@ -0,0 +1,14 @@
+package com.abhijeet.travel_saathi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_successfully extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_successfully);
+    }
+}
