94c920b1fb3fad857dd30171141107b6bf9333b1
Aryan-Sood <aryansood12@gmail.com>
2024-02-26 18:22:45 +0530

    Home Page V2

---
+++ app/src/main/java/com/abhijeet/travel_saathi/utilities/GradientTextView.java
@@ -0,0 +1,50 @@
+package com.abhijeet.travel_saathi.utilities;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class GradientTextView extends AppCompatTextView {
+
+    private int firstColor;
+    private int secondColor;
+
+    public GradientTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    public GradientTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public GradientTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        firstColor = 0xFF000000;
+        secondColor = 0xFF5754FF;
+    }
+
+    public void setGradientColors(int startColor, int endColor) {
+        this.firstColor = startColor;
+        this.secondColor = endColor;
+        invalidate();
+    }
+
+    @SuppressLint("DrawAllocation")
+    @Override
+    protected void onDraw(android.graphics.Canvas canvas) {
+        getPaint().setShader(new LinearGradient(
+                0, 0, getWidth(), getHeight(),
+                new int[]{firstColor, secondColor}, // Example gradient colors
+                null, Shader.TileMode.CLAMP));
+        super.onDraw(canvas);
+    }
+}
