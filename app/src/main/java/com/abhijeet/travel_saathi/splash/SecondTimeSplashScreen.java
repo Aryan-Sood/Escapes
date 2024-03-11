d6667199d89199d64172b1f60488fb26a8e63009
Vibhor Mathur <matvibhor24@gmail.com>
2024-03-11 20:09:25 +0530

    updated - SecondTimeSplashScreen

--- app/src/main/java/com/abhijeet/travel_saathi/splash/SecondTimeSplashScreen.java
+++ app/src/main/java/com/abhijeet/travel_saathi/splash/SecondTimeSplashScreen.java
@@ -1,138 +1,81 @@
 package com.abhijeet.travel_saathi.splash;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.motion.widget.MotionLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
 import com.abhijeet.travel_saathi.R;
 import com.abhijeet.travel_saathi.activities.Home_page;
 
 public class SecondTimeSplashScreen extends AppCompatActivity {
-    MotionLayout motionLayout;
+    private MotionLayout motionLayout;
+    private int[] transitionIds = {
+            R.id.justScooter,
+            R.id.findYourCompanyEnter,
+            R.id.chooseADateEnter,
+            R.id.travelTheWorldEntry,
+            R.id.jitter1,
+            R.id.jitter2,
+            R.id.jitter3,
+            R.id.jitter1,
+            R.id.jitter4,
+            R.id.travelTheWorldExit,
+            R.id.chooseADateExit,
+            R.id.findYourCompanyExit,
+            R.id.end
+    };
+    private int currentTransitionIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_time_splash_screen);
 
         motionLayout = findViewById(R.id.justScooterUp);
+        motionLayout.setTransitionListener(transitionListener);
 
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                motionLayout.transitionToState(transitionIds[currentTransitionIndex]);
+            }
+        }, 500); // Adjust the delay as needed
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        // Trigger your transition here
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.justScooter);
-            }
-        }, 500);
+    private MotionLayout.TransitionListener transitionListener = new MotionLayout.TransitionListener() {
+        @Override
+        public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
+        }
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.findYourCompanyEnter);
-            }
-        }, 1200);
+        @Override
+        public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+        }
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.chooseADateEnter);
-            }
-        }, 2000);
+        @Override
+        public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.travelTheWorldEntry);
+            if (currentTransitionIndex < transitionIds.length - 1) {
+                currentTransitionIndex++;
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        motionLayout.transitionToState(transitionIds[currentTransitionIndex]);
+                    }
+                }, 500);
             }
-        }, 2600);
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.jitter1);
+            else {
+                startActivity(new Intent(SecondTimeSplashScreen.this, Home_page.class));
+                finish();
             }
-        }, 3200);
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.jitter2);
-            }
-        }, 3800);
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.jitter3);
-            }
-        }, 4400);
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.jitter1);
-            }
-        }, 5000);
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.jitter4);
-            }
-        }, 5600);
+        }
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.travelTheWorldExit);
-            }
-        }, 6200);
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.chooseADateExit);
-            }
-        }, 6800);
-
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.findYourCompanyExit);
-            }
-        }, 7400);
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                motionLayout.transitionToState(R.id.end);
-            }
-        }, 8000);
-
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Trigger your transition after the delay
-                Intent intent = new Intent(SecondTimeSplashScreen.this, Home_page.class);
-                startActivity(intent);
-            }
-        }, 8250);
-
-
-    }
+        @Override
+        public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
+        }
+    };
 }
