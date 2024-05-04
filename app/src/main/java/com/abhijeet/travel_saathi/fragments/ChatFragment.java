47b480b022e1032f11bc30cc2c4b91ae741a6653
Siddharth Kumar <99739245+Sid435@users.noreply.github.com>
2024-05-04 16:46:02 +0530

    bug fix

--- app/src/main/java/com/abhijeet/travel_saathi/fragments/ChatFragment.java
+++ app/src/main/java/com/abhijeet/travel_saathi/fragments/ChatFragment.java
@@ -13,13 +13,23 @@ import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.abhijeet.travel_saathi.R;
+import com.abhijeet.travel_saathi.adapters.ChatMessageRecyclerAdapter;
+import com.abhijeet.travel_saathi.models.ChatroomModel;
+import com.abhijeet.travel_saathi.utils.FirebaseUtil;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.firestore.Query;
 
 public class ChatFragment extends BottomSheetDialogFragment {
+
+    RecyclerView recyclerView;
+    ChatMessageRecyclerAdapter adapter;
 
 
     @NonNull
@@ -45,15 +55,53 @@ public class ChatFragment extends BottomSheetDialogFragment {
         return dialog;
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.chat_fragment_layout, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.chat_fragment_layout, container, false);
+        recyclerView = view.findViewById(R.id.chatsRecyclerView);
+        setupRecyclerView();
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    void setupRecyclerView(){
+        Query query = FirebaseUtil.allChatroomCollectionReference()
+                .whereArrayContains("userIds",FirebaseUtil.currentUserId())
+                .orderBy("lastMessageTimestamp",Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<ChatroomModel> options = new FirestoreRecyclerOptions.Builder<ChatroomModel>()
+                .setQuery(query,ChatroomModel.class).build();
+
+        adapter = new ChatMessageRecyclerAdapter(options,getContext());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(adapter!=null)
+            adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(adapter!=null)
+            adapter.stopListening();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(adapter!=null)
+            adapter.notifyDataSetChanged();
     }
 }
