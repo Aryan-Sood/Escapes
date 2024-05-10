7c36ffcbc5e90475c800b5cbe38e5746be24a552
Siddharth Kumar <99739245+Sid435@users.noreply.github.com>
2024-05-10 17:02:27 +0530

    bug fix

--- app/src/main/java/com/abhijeet/travel_saathi/models/SuggestedPlacesModelClass.java
+++ app/src/main/java/com/abhijeet/travel_saathi/models/SuggestedPlacesModelClass.java
@@ -10,16 +10,8 @@ public class SuggestedPlacesModelClass {
         this.sceneName = sceneName;
     }
 
-    public void setSceneImage(int sceneImage) {
-        this.sceneImage = sceneImage;
-    }
-
     public String getSceneName() {
         return sceneName;
-    }
-
-    public void setSceneName(String sceneName) {
-        this.sceneName = sceneName;
     }
 
     public int getSceneImage() {
